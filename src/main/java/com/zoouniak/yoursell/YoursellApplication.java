5ce4c76df6d8974d4d5b7c87beb806c190c3ba34
jueun oh <jueun1025@naver.com>
2023-10-02 11:06:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/zoouniak/yoursell/YoursellApplication.java
@@ -0,0 +1,13 @@
+package com.zoouniak.yoursell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoursellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoursellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoouniak/yoursell/YoursellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoouniak.yoursell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoursellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
