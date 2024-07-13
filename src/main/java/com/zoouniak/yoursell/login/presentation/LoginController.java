4dc59d43352fc4e2c44fa56e8c6a0771067566f7
jueun oh <jueun1025@naver.com>
2024-07-13 20:51:23 +0900

    fix : 어세스토큰 헤더로 변경, 로그인 연장 추가

--- src/main/java/com/zoouniak/yoursell/login/presentation/LoginController.java
+++ src/main/java/com/zoouniak/yoursell/login/presentation/LoginController.java
@@ -1,21 +1,23 @@
 package com.zoouniak.yoursell.login.presentation;
 
+import com.zoouniak.yoursell.login.dto.request.LoginRequest;
 import com.zoouniak.yoursell.login.dto.response.AccessTokenResponse;
 import com.zoouniak.yoursell.login.dto.response.AuthTokens;
-import com.zoouniak.yoursell.login.dto.request.LoginRequest;
 import com.zoouniak.yoursell.login.service.LoginService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.SET_COOKIE;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
+    private static final String REFRESH_TOKEN = "refresh-token";
+    private static final String ACCESS_TOKEN = "access-token";
     private static final int COOKIE_AGE_SECONDS = 604800;
     private final LoginService loginService;
 
@@ -25,23 +27,37 @@ public class LoginController {
     @PostMapping("/login/{provider}")
     public ResponseEntity<AccessTokenResponse> login(
             @PathVariable(name = "provider") final String provider,
-            @RequestBody final LoginRequest loginRequest,
-            HttpServletResponse response) {
+            @RequestBody final LoginRequest loginRequest
+    ) {
         AuthTokens tokens = loginService.login(provider, loginRequest);
 
-        response.addHeader(SET_COOKIE, makeCookie(tokens.refreshToken()).toString());
-        return ResponseEntity.status(CREATED).body(new AccessTokenResponse(tokens.accessToken()));
+        return ResponseEntity.
+                status(CREATED)
+                .header(SET_COOKIE, makeCookie(tokens.refreshToken()).toString())
+                .header(ACCESS_TOKEN, tokens.accessToken())
+                .build();
+    }
+
+    @PostMapping("/login/extend")
+    public ResponseEntity<?> extendLogin(
+            @CookieValue(name = REFRESH_TOKEN) final String refreshToken,
+            @RequestHeader(name = AUTHORIZATION) final String authorizeHeader
+
+    ) {
+        AccessTokenResponse accessToken = loginService.extend(authorizeHeader, refreshToken);
+        return ResponseEntity.status(CREATED)
+                .header(ACCESS_TOKEN, accessToken.accessToken())
+                .build();
     }
 
     private static ResponseCookie makeCookie(final String refreshToken) {
-        return ResponseCookie.from("refresh-token", refreshToken)
+
+        return ResponseCookie.from(REFRESH_TOKEN, refreshToken)
                 .maxAge(COOKIE_AGE_SECONDS)
                 .sameSite("None")
-                .secure(true)
+                .secure(false)
                 .httpOnly(true)
                 .path("/")
                 .build();
     }
-
-
 }
