19b1fb457ce844da59824939753068a589b4003e
jueun oh <jueun1025@naver.com>
2024-07-16 00:19:50 +0900

    feat : 구글 소셜 로그인 구현

---
+++ src/main/java/com/zoouniak/yoursell/login/infra/oauthUserInfo/GoogleOAuthUserInfo.java
@@ -0,0 +1,24 @@
+package com.zoouniak.yoursell.login.infra.oauthUserInfo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record GoogleOAuthUserInfo(
+        @JsonProperty("id")
+        String socialId,
+        @JsonProperty("email")
+        String email,
+        @JsonProperty("name")
+        String name
+) implements OAuthUserInfo {
+    @Override
+    public String getLoginId() {
+        return socialId();
+    }
+
+    @Override
+    public String getNickname() {
+        return name();
+    }
+
+
+}
